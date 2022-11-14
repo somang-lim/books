e8f418501e5ce1b045d95e82d627511b6539689b
somang-lim <somangjuha@gmail.com>
2022-11-14 16:59:31 +0900

    chore: create db init.sql & change application datasource url

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+# 테스트 DB 생성
+DROP DATABASE IF EXISTS imbooks_test;
+CREATE DATABASE imbooks_test;
+USE imbooks_test;
+
+# 개발 DB 생성
+DROP DATABASE IF EXISTS imbooks_dev;
+CREATE DATABASE imbooks_dev;
+Use imbooks_dev;
