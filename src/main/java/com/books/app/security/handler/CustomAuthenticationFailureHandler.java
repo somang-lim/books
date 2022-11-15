bfdaab9b71ed06c0559f7c0acdcbc40a713b4260
somang-lim <somangjuha@gmail.com>
2022-11-16 00:24:53 +0900

    refactor: 시큐리티 실패 핸들러 수정 (#1)

--- src/main/java/com/books/app/security/handler/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/books/app/security/handler/CustomAuthenticationFailureHandler.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
-import com.books.util.Ut;
+import com.books.app.base.rq.Rq;
 
 @Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
@@ -20,8 +20,8 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-										AuthenticationException exception) throws IOException, ServletException {
-		String url = "/member/login?errorMsg=" + Ut.url.encode("아이디나 비밀번호가 일치하지 않습니다. 다시 로그인해주세요.");
+										AuthenticationException exception) throws IOException {
+		String url = Rq.urlWithMsg("/member/login", "로그인 정보가 올바르지 않습니다.");
 
 		redirectStrategy.sendRedirect(request, response, url);
 	}
